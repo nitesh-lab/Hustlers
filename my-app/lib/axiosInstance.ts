3817bd87df8279fd478c1b6f5239e340c0c800ea
nitesh <niteshsemwal15@gmail.com>
2024-07-04 21:24:26 +0530

    v1_

--- my-app/lib/axiosInstance.ts
+++ my-app/lib/axiosInstance.ts
@@ -1,11 +1,11 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//     baseURL: 'hustlers-production.up.railway.app/',
-//   });
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'https://hustlers.onrender.com/',
   });
+
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:8000/',
+//   });
