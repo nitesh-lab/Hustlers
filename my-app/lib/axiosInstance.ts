ba5b24a5d621b118aef341241acfff970de742f0
nitesh <niteshsemwal15@gmail.com>
2024-07-04 21:55:03 +0530

    v1__2:

--- my-app/lib/axiosInstance.ts
+++ my-app/lib/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://hustlers.onrender.com/',
+    baseURL: "https://hustler-backend.onrender.com/",
   });
